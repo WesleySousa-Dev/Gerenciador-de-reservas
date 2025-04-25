9431f60618b14130dab8199de13eef9a15cf0683
Wesleyy06 <wesleydesousa34@gmail.com>
2025-04-25 15:23:52 -0300

    início do projeto de gerenciamento de reservas

---
+++ src/main/java/io/github/wesleyy06/Gerenciador_de_reservas/GerenciadorDeReservasApplication.java
@@ -0,0 +1,13 @@
+package io.github.wesleyy06.Gerenciador_de_reservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeReservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeReservasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/wesleyy06/Gerenciador_de_reservas/GerenciadorDeReservasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wesleyy06.Gerenciador_de_reservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeReservasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
