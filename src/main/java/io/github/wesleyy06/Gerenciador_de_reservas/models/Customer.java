2fe5865cdd6aa430ddbd04183a8081043a41ddca
Wesleyy06 <wesleydesousa34@gmail.com>
2025-04-26 12:03:20 -0300

    feat: definição de atributos

--- src/main/java/io/github/wesleyy06/Gerenciador_de_reservas/models/Customer.java
+++ src/main/java/io/github/wesleyy06/Gerenciador_de_reservas/models/Customer.java
@@ -1,4 +1,7 @@
 package io.github.wesleyy06.Gerenciador_de_reservas.models;
 
 public class Customer {
+    private String name;
+    private String lastname;
+    private long phoneNumber;
 }
--- src/main/java/io/github/wesleyy06/Gerenciador_de_reservas/models/Reserve.java
+++ src/main/java/io/github/wesleyy06/Gerenciador_de_reservas/models/Reserve.java
@@ -1,4 +1,9 @@
 package io.github.wesleyy06.Gerenciador_de_reservas.models;
 
-public class Reserve {
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Reserve extends Customer{
+    private LocalDate reserveDate;
+    private LocalTime reservationTime;
 }
