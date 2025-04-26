b5f97473d9f2c486bd21f7fc0a1ee58a93104300
Wesleyy06 <wesleydesousa34@gmail.com>
2025-04-26 15:16:29 -0300

    feat: definição de construtores e um método toString

--- src/main/java/io/github/wesleyy06/Gerenciador_de_reservas/models/Reserve.java
+++ src/main/java/io/github/wesleyy06/Gerenciador_de_reservas/models/Reserve.java
@@ -6,4 +6,28 @@ import java.time.LocalTime;
 public class Reserve extends Customer{
     private LocalDate reserveDate;
     private LocalTime reservationTime;
+
+    public Reserve(LocalDate reserveDate, LocalTime reservationTime) {
+        this.reserveDate = reserveDate;
+        this.reservationTime = reservationTime;
+    }
+
+    public Reserve(String name, String lastname, long phoneNumber, LocalDate reserveDate, LocalTime reservationTime) {
+        super(name, lastname, phoneNumber);
+        this.reserveDate = reserveDate;
+
+        if(reservationTime == null) {
+            System.out.println("Horario mencianado invalido!");
+        } else {
+            this.reservationTime = reservationTime;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + getName() + ' ' + getLastname()+
+                "Telefone/celular: " + getPhoneNumber() +
+                "Data reserva: " + reserveDate +
+                "Horario marcado: " + reservationTime;
+    }
 }
