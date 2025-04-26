2fbe3c236562573f39f102d147f1ffe0e3077db1
Wesleyy06 <wesleydesousa34@gmail.com>
2025-04-26 15:13:20 -0300

    feat: definição de getters

--- src/main/java/io/github/wesleyy06/Gerenciador_de_reservas/models/Customer.java
+++ src/main/java/io/github/wesleyy06/Gerenciador_de_reservas/models/Customer.java
@@ -4,4 +4,25 @@ public class Customer {
     private String name;
     private String lastname;
     private long phoneNumber;
+
+    public Customer() {
+    }
+
+    public Customer(String name, String lastname, long phoneNumber) {
+        this.name = name;
+        this.lastname = lastname;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
 }
